c7e3facaaa0d57a92898f222f563bc928b351562
AvBorikDeveloper <avboris28@gmail.com>
2019-04-17 22:45:00 +1200

    Generating the token (postman)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Validator;
 use Illuminate\Http\Request;
 
@@ -18,5 +19,14 @@ class UserController extends Controller
         if($validator->fails()){
             return response()->json(['error'=>$validator->errors()], 401);
         }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+
+        $user = User::create($input);
+
+        $success['token'] = $user->createToken('Lararest aut')->accessToken;
+
+        return response()->json(['success'=> $success], 200);
     }
 }
