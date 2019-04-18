85a3c540ecf5c198af24625ab765c11e596df7d4
AvBorikDeveloper <avboris28@gmail.com>
2019-04-19 01:02:29 +1200

    Sending email with postman

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -10,18 +10,18 @@ class EmailController extends Controller
     public function contact(Request $request) {
         $input = $request->all();
         $email = $input['email'];
-        $email = $input['name'];
-        $email = $input['body'];
+        $name = $input['name'];
+        $body = $input['body'];
 
         Mail::send('emails.contact',[
             'email'=> $email,
             'name'=> $name,
             'body'=> $body
-        ],function($message){
-            $message->from('user@gmail.com','contact page');
+        ],function($message) use($email) {
+            $message->from($email,'contact page');
             $message->to('bba8ee446d-bfa052@inbox.mailtrap.io');
         });
 
-        return responce()->json(['message'=>'request completed']);
+        return response()->json(['message'=>'request completed']);
     }
 }
